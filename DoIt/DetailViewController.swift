61d2e3813e2b4c334b073f195c0927475aed6b13
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-02-13 13:22:03 -0800

    initial commit

---
+++ DoIt/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  DoIt
+//
+//  Created by jason harrison on 2019-02-13.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.todoDescription!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: ToDo? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
